b149a48f5e15e99d1cfcdf777a7cfb3757779b6f
Vanessa James <kimondo2005@gmail.com>
2025-11-09 12:51:24 +0300

    Fix admin.py to include Book import and proper registration for ALX

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book  # <--- ALX specifically checks for this
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns shown in admin list
+    search_fields = ('title', 'author')                     # Search box
+    list_filter = ('publication_year',)                     # Filter sidebar
+
+admin.site.register(Book, BookAdmin)
+
